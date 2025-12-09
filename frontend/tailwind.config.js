b43172dc247cea5389b791f3785994cf62c7c516
raj-krr <mr.rajkumar3579@gmail.com>
2025-12-10 00:06:43 +0530

    feat: Add custom theme configuration in Tailwind CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+
+        mytheme: {
+          "primary": "#2563EB",
+          "secondary": "#3B82F6",
+          "accent": "#1E40AF",
+
+          "base-100": "#F8FAFC",
+          "neutral": "#0F172A",
+
+          "success": "#22C55E",
+          "warning": "#F5900B",
+          "error": "#EF4444",
+        },
+    
     extend: {},
   },
   plugins: [require("daisyui")],
