e27d9a617f8356e76172cd50a45d04b4f5defd1f
raj-krr <mr.rajkumar3579@gmail.com>
2026-03-19 11:04:01 +0530

    added more ice candidates

--- frontend/src/components/call/hooks/useCall.ts
+++ frontend/src/components/call/hooks/useCall.ts
@@ -13,9 +13,19 @@ export function useCall() {
   const createPeer = (remoteId: string) => {
     const peer = new RTCPeerConnection({
       iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" },
-      ],
+  { urls: "stun:stun.l.google.com:19302" },
+
+  {
+    urls: "turn:openrelay.metered.ca:80",
+    username: "openrelayproject",
+    credential: "openrelayproject",
+  },
+  {
+    urls: "turn:openrelay.metered.ca:443",
+    username: "openrelayproject",
+    credential: "openrelayproject",
+  },
+]
     });
 
     peer.onicecandidate = (e) => {
